0772b9d3b5ac80b299670d0d0a97600f838d5dd3
Gil Maimon <gilm2121@gmail.com>
2019-01-15 18:45:30 +0200

    started changing code and structure to support react frontend

--- server/main.js
+++ server/main.js
@@ -1,20 +1,17 @@
 let fs = require('fs');
 let path = require('path');
 
-let app = require('express')();
+let express = require('express');
+let app = express();
 let request = require('request');
 
 let config = require('./config');
 
 let db = require('./db')(config.mongodb_path, config.db_name);
-
-//services
 let libraries_reloader = require('./utilities/reload_libraries.js')
-
-
 let badgeUrl = require('./utilities/badge');
 let libraries = require('./libraries');
-let access_logger = require('./utilities/access_log');
+let access_logger = require('./access_log');
 
 libraries_reloader.onInterval(1000 * config.libraries_refresh_interval_in_seconds, function(err) {
     if(err) {
@@ -22,14 +19,12 @@ libraries_reloader.onInterval(1000 * config.libraries_refresh_interval_in_second
     } else {
         console.log("Succesfuly updated libraries");
     }
-});
-
-access_logger.cacheOnInterval(1000 * config.stats_refresh_interval_in_seconds);
+})
 
 let arduinoLogoBase64 = function(filename){
     var bitmap = fs.readFileSync(filename);
     return new Buffer(bitmap).toString('base64');
-}(path.join(__dirname, '../res', 'arduino_logo_tiny.png'));
+}(path.join(__dirname, '/res', 'arduino_logo_tiny.png'));
 
 function getSvgFromUrl(url) {
     return new Promise(function(resolve, reject) {
@@ -40,19 +35,18 @@ function getSvgFromUrl(url) {
     })
 }
 
-app.get('/badge/:libname.svg', async function(req, res) {
-    let libname = req.params.libname;
+app.use(express.static(path.join(__dirname, 'ui/build')));
+app.get('/badge/:repo.svg', async function(req, res) {
+    access_logger.logEntry(req);
+
+    // get repo name
+    let repoName = req.params.repo;
     
     // fetch a library with that name
-    let library = await libraries.getMostRecent(libname);
+    let library = await libraries.getMostRecent(repoName);
     
-    // log entry if it was for an existing library
-    if(library) {
-        access_logger.logEntry(req, "ACTION_GET_BADGE");
-    }
-
     // create appropriate badge url
-    resPath = badgeUrl(arduinoLogoBase64, libname, library);
+    resPath = badgeUrl(arduinoLogoBase64, repoName, library);
     
     // serve that badge (svg file) to the user
     let svg = await getSvgFromUrl(resPath);
@@ -62,16 +56,12 @@ app.get('/badge/:libname.svg', async function(req, res) {
 });
 
 app.get('/library/:libname', async function(req, res) {
+    // get repo name
     let libname = req.params.libname;
 
     // fetch a library with that name
     let library = await libraries.getMostRecent(libname);
     
-    // log entry if it was for an existing library
-    if(library) {
-        access_logger.logEntry(req, "ACTION_GET_LIBDATA");
-    }
-
     let result = {};
     if(library) {
         result.data = library;
@@ -83,21 +73,8 @@ app.get('/library/:libname', async function(req, res) {
     res.json(result);
 })
 
-// get most watched
-app.get('/stats/top/week', function(req, res) {
-    //return most watched libraries in the last week
-    res.send("NotImplemented");
-})
-
-// get recently added
-app.get('/stats/recent', async function(req, res) {
-    //return the last libraries to be acccessed for the first time
-    try {
-        let recentlyWatched = await access_logger.recentNewLibnames(10);
-        res.json(recentlyWatched);
-    } catch(err) {
-        res.json([]);
-    }
-})
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui/public/index.html'))
+});
 
 app.listen(config.port, '0.0.0.0');
