d2339d60a9afcc9f9259c65275f27df3f24d88ab
Gil Maimon <gilm2121@gmail.com>
2019-01-16 23:36:49 +0200

    hand-mergeded main.js

--- server/main.js
+++ server/main.js
@@ -6,12 +6,13 @@ let app = express();
 let request = require('request');
 
 let config = require('./config');
-
 let db = require('./db')(config.mongodb_path, config.db_name);
-let libraries_reloader = require('./utilities/reload_libraries.js')
 let badgeUrl = require('./utilities/badge');
 let libraries = require('./libraries');
-let access_logger = require('./access_log');
+
+//services
+let libraries_reloader = require('./utilities/reload_libraries.js')
+let access_logger = require('./utilities/access_log');
 
 libraries_reloader.onInterval(1000 * config.libraries_refresh_interval_in_seconds, function(err) {
     if(err) {
@@ -19,7 +20,9 @@ libraries_reloader.onInterval(1000 * config.libraries_refresh_interval_in_second
     } else {
         console.log("Succesfuly updated libraries");
     }
-})
+});
+
+access_logger.cacheOnInterval(1000 * config.stats_refresh_interval_in_seconds);
 
 let arduinoLogoBase64 = function(filename){
     var bitmap = fs.readFileSync(filename);
@@ -35,18 +38,22 @@ function getSvgFromUrl(url) {
     })
 }
 
+// for static react files
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('/badge/:repo.svg', async function(req, res) {
-    access_logger.logEntry(req);
 
-    // get repo name
-    let repoName = req.params.repo;
+app.get('/badge/:libname.svg', async function(req, res) {
+    let libname = req.params.libname;
     
     // fetch a library with that name
-    let library = await libraries.getMostRecent(repoName);
+    let library = await libraries.getMostRecent(libname);
     
+    // log entry if it was for an existing library
+    if(library) {
+        access_logger.logEntry(req, "ACTION_GET_BADGE");
+    }
+
     // create appropriate badge url
-    resPath = badgeUrl(arduinoLogoBase64, repoName, library);
+    resPath = badgeUrl(arduinoLogoBase64, libname, library);
     
     // serve that badge (svg file) to the user
     let svg = await getSvgFromUrl(resPath);
@@ -56,12 +63,16 @@ app.get('/badge/:repo.svg', async function(req, res) {
 });
 
 app.get('/library/:libname', async function(req, res) {
-    // get repo name
     let libname = req.params.libname;
 
     // fetch a library with that name
     let library = await libraries.getMostRecent(libname);
     
+    // log entry if it was for an existing library
+    if(library) {
+        access_logger.logEntry(req, "ACTION_GET_LIBDATA");
+    }
+
     let result = {};
     if(library) {
         result.data = library;
@@ -73,8 +84,26 @@ app.get('/library/:libname', async function(req, res) {
     res.json(result);
 })
 
+// get most watched
+app.get('/stats/top/week', function(req, res) {
+    //return most watched libraries in the last week
+    res.send("NotImplemented");
+})
+
+// get recently added
+app.get('/stats/recent', async function(req, res) {
+    //return the last libraries to be acccessed for the first time
+    try {
+        let recentlyWatched = await access_logger.recentNewLibnames(10);
+        res.json(recentlyWatched);
+    } catch(err) {
+        res.json([]);
+    }
+});
+
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
 });
+
 
 app.listen(config.port, '0.0.0.0');
