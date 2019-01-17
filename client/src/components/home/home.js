12fa12049e44e6c9bfbd81c5b04e17f2fe0b2824
Gil Maimon <gilm2121@gmail.com>
2019-01-17 10:24:08 +0200

    worked on home page and added recently-list basics

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -6,6 +6,8 @@ import './home.css'
 import Header from '../common/header'
 import Footer from '../common/footer'
 
+import RecentlyStartedWatching from './recently'
+
 class InteractiveBadgeInput extends Component {
     constructor(props) {
         super(props);
@@ -66,23 +68,28 @@ class Home extends Component {
                 <div class="homeWrapper">
                 <div class="leftContent">
                     <h2>What is ArduBadge?</h2>
-                    <p>Arduino Library Manger Badge is a way to help new and experienced
-                        users to get a quick overview and installation guide for community-contributed 
-                        Arduino libraries.
+                    <p>Arduino Library Manger Badge (or <span class="homeSpan">ArduBadge</span>) is a way to help new and experienced
+                        users to get a quick overview and installation guide for community-contributed <span class="homeSpan">Arduino</span> libraries.
                     </p>
                     <p>
-                        With ArduBadge you get a custom and dynamic markdown (GitHub) 
+                        With <span class="homeSpan">ArduBadge</span> you get a custom and dynamic markdown (GitHub) 
                         badge that elegently shows users that your library is available 
-                        via the built in Library Manager. In one click they can view 
+                        via the built in <span class="homeSpan">Library Manager</span>. In one click they can view 
                         instruction to help them trough the installtion proccess.
                     </p>
+                    <a href="/MySensors/ide">
+                        <h3>Click Here to see an example!</h3>
+                    </a>
                     <br/>
                     <div>
                         <h2>Go Badge Yourself</h2>
-                        <p>You have created an Arduino Library? ArduBadge is completly free
+                        <p>You have created an Arduino Library? <span class="homeSpan">ArduBadge</span> is completly free
                             and it is really easy to get your own custom badge.
                             Type your library name below and copy the badge markdown 
                             to your README.md or website.
+                        </p>
+                        <p>
+                            Clicking on the badge will get you to your own custom installation guide!
                         </p>
                         <InteractiveBadgeInput />
                     </div>
@@ -90,7 +97,8 @@ class Home extends Component {
                 <div class="rightContent">
                     <p>
                         This is nothing important this is just here to 
-                        take some room on the right side
+                        take some room on the right side.
+                        <RecentlyStartedWatching />
                     </p>
                 </div>
                 </div>
---
+++ client/src/components/home/recently.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './recently.css'
+
+class RecentlyStartedWatching extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                Empty For Now
+            </div>
+        )
+    }
+}
+
+export default RecentlyStartedWatching;
