544009192a2d36ced395362c0eab4508b023a884
Gil Maimon <gilm2121@gmail.com>
2019-01-17 10:06:39 +0200

    added interactive badge and content to home screen

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BeatLoader } from 'react-spinners';
+import AutosizeInput from 'react-input-autosize';
 
 import './home.css'
 
@@ -9,15 +9,41 @@ import Footer from '../common/footer'
 class InteractiveBadgeInput extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+
+        const defaultName = "MyLibrary";
+        this.domainHttpPrefix = "http://ardu-badge.xyz";
+        this.onLibnameChange = this.onLibnameChange.bind(this);
+        this.state = {
+            libname: defaultName
+        }
+    }
+
+    onLibnameChange(e) {
+        this.setState({
+            libname: e.target.value
+        });
     }
 
     render() {
         return (
             <div>
-                <span>http://localhost/badge/</span>
-                <input></input>
-                <span>.svg</span>
+                
+                <div class="interactiveWrapper">
+                    <a class="badgePrefix">{this.domainHttpPrefix}/badge/</a>
+                    <AutosizeInput
+                        style={{ fontSize: 18 }}
+                        class="badgeInput" 
+                        onChange={this.onLibnameChange} 
+                        value={this.state.libname}/>
+                    <a class="badgePostfix">.svg</a>
+                    <a class="badgeWrapper" href={`/${this.state.libname}/ide`}>
+                        <img class="badgeImg" src={`/badge/${this.state.libname}.svg`}/>
+                    </a>
+                </div>
+                <h3>Use This Markdown</h3>
+                <p class="markdownCode">
+                    ![![arduino-library-badge]({this.domainHttpPrefix}/badge/{this.state.libname}.svg)]({this.domainHttpPrefix}/{this.state.libname})
+                </p>
             </div>
         );
     }
@@ -37,8 +63,36 @@ class Home extends Component {
         return (
             <div>
                 <Header title="ArduBadge"/>
-                <div class="content homeWrapper">
-                    <InteractiveBadgeInput />
+                <div class="homeWrapper">
+                <div class="leftContent">
+                    <h2>What is ArduBadge?</h2>
+                    <p>Arduino Library Manger Badge is a way to help new and experienced
+                        users to get a quick overview and installation guide for community-contributed 
+                        Arduino libraries.
+                    </p>
+                    <p>
+                        With ArduBadge you get a custom and dynamic markdown (GitHub) 
+                        badge that elegently shows users that your library is available 
+                        via the built in Library Manager. In one click they can view 
+                        instruction to help them trough the installtion proccess.
+                    </p>
+                    <br/>
+                    <div>
+                        <h2>Go Badge Yourself</h2>
+                        <p>You have created an Arduino Library? ArduBadge is completly free
+                            and it is really easy to get your own custom badge.
+                            Type your library name below and copy the badge markdown 
+                            to your README.md or website.
+                        </p>
+                        <InteractiveBadgeInput />
+                    </div>
+                </div>
+                <div class="rightContent">
+                    <p>
+                        This is nothing important this is just here to 
+                        take some room on the right side
+                    </p>
+                </div>
                 </div>
                 <Footer />
             </div>
