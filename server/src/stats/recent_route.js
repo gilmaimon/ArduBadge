a8ad0d3aeed9c84390926de18a907e5151cb7b15
Gil Maimon <gilm2121@gmail.com>
2019-01-22 19:04:47 +0200

    replaced recent implementation to not use try, catch and use then,catch instead

--- server/src/stats/recent_route.js
+++ server/src/stats/recent_route.js
@@ -3,12 +3,9 @@ module.exports = {
         // get recently added
         app.get('/stats/recent', async function(req, res) {
             //return the last libraries to be acccessed for the first time
-            try {
-                let recentlyWatched = await access_logger.recentNewLibnames(10);
-                res.json(recentlyWatched);
-            } catch(err) {
-                res.json([]);
-            }
+            access_logger.recentNewLibnames(10)
+                .then((recentlyWatched) => res.json(recentlyWatched))
+                .catch((err) => res.json([]));
         });
     }
 }
